d11071132b9600742286b9eb767cfc1722b97ec1
125053864-arch <125053864@upq.edu.mx>
2026-09-18 18:50:36 -0600

    avance de modulo individual hasta este momnento: solo cree las clases y menu
    en esta parte lo que pude crear es las clases y un intento de hacer un menu

---
+++ CofeCode/Cocina.js
@@ -0,0 +1,45 @@
+
+const Agregar = {
+  nombre_Producto: "hamburguesa",
+  numero_producto: 3,
+  saludar: function() {
+    console.log("Se agrego correctamente el producto ");
+  }
+};
+
+const editar = {
+  cambiar_nombre: "se cambiara el nombre",
+  cambiar_num_producto: "se cambiara el numero del producto",
+  edicion: function() {
+    console.log("Se edito correctamente");
+  }
+};
+
+const Eliminar = {
+  Elimnar: "se eliminara",
+  eliminar: function() {
+    console.log("Se elimino correctamente");
+  }
+}
+
+const Listar = {
+  enlistara: "se enlistara",
+  eliminar: function() {
+    console.log("Se enlisto correctamente");
+  }
+}
+
+
+
+
+
+function Cocina(){
+    console.log(
+        1 = Agregar,
+        2 = editar,
+        3 = Eliminar,
+        4 = Listar
+
+    );
+}
+
