dc8c474ab0f8f4106b885082b9fa151a020ac7e0
125053864-arch <125053864@upq.edu.mx>
2026-09-19 19:07:09 -0600

    finalizacion de etapa individual Cocina

--- CofeCode/Cocina.js
+++ CofeCode/Cocina.js
@@ -1,45 +1,116 @@
+let readline = require('readline');
+let teclado = readline.createInterface({
+    input: process.stdin, 
+    output: process.stdout
+});
 
-const Agregar = {
-  nombre_Producto: "hamburguesa",
-  numero_producto: 3,
-  saludar: function() {
-    console.log("Se agrego correctamente el producto ");
-  }
-};
+let productos = [];
 
-const editar = {
-  cambiar_nombre: "se cambiara el nombre",
-  cambiar_num_producto: "se cambiara el numero del producto",
-  edicion: function() {
-    console.log("Se edito correctamente");
-  }
-};
+function mostrarMenu(){
+    console.log(`
+       MENU 
+       1. agregar
+       2. Editar
+       3. Eliminar
+       4. listar productos 
+       5. Salir
+    `);
+    teclado.question("Seleccione una opcion: ", function(opcion)  {
+        if (opcion == "1"){
+            agregarProducto();
 
-const Eliminar = {
-  Elimnar: "se eliminara",
-  eliminar: function() {
-    console.log("Se elimino correctamente");
-  }
+        }else if (opcion == "2"){
+
+            editarProducto();
+
+        }else if (opcion == "3"){
+            eliminarProducto();
+
+        }else if (opcion == "4") {
+            listarProductos();
+            mostrarMenu();
+        }else if (opcion == "5"){
+            console.log("Saliendo del menu");
+            teclado.close();
+        }else {
+            console.log("Opcion invalida");
+            mostrarMenu();
+        }
+    });
+}
+function agregarProducto(){
+    teclado.question("Nombre del producto: ", function(nombre){
+        teclado.question("precio deel producto: ",function(precio){
+            let producto = {
+                nombre: nombre,
+                precio: Number(precio)
+            };
+            productos.push(producto);
+
+            console.log("producto agregado");
+            mostrarMenu();
+
+        
+        });
+    })
 }
 
-const Listar = {
-  enlistara: "se enlistara",
-  eliminar: function() {
-    console.log("Se enlisto correctamente");
-  }
+function editarProducto(){
+    listarProductos();
+
+    teclado.question("Ingres el numero del producto a editar: ", function(numero) {
+
+        let indice = numero -1;
+       
+        teclado.question("NUEVO nombre: ", function(nombre) {
+            teclado.question("Nuevo precio: ", function(precio){
+
+                productos[indice].nombre = nombre;
+                productos[indice].precio = Number(precio);
+
+                console.log("Producto editado.");
+                mostrarMenu();
+
+            });
+        });
+    });
+}
+function eliminarProducto(){
+    listarProductos();
+    teclado.question("ingrese el nuemero del a eliminar: ", function(numero){
+
+        let indice = numero -1;
+        
+        productos.splice(indice, 1);
+
+        console.log("Producto eliminado.")
+        mostrarMenu();
+    })
+}
+function listarProductos(){
+    if (productos.length == 0) {
+        console.log("No hay productos registrados.");
+
+    } else {
+        console.log(`
+            PRODUCTOS REGISTRADOS
+
+        `);
+
+        for (let i =0; i < productos.length; i++){
+            console.log(
+                (i +1) + ". " +
+                productos[i].nombre +
+                "-$"+
+                productos[i].precio
+            );
+        }
+
+        
+    }
 }
 
+mostrarMenu();
 
 
-
-
-function Cocina(){
-    console.log(
-        1 = Agregar,
-        2 = editar,
-        3 = Eliminar,
-        4 = Listar
-
-    );
-}
 
