fb79afeb8af99125f7ccd6da488b880d07a291cd
125053864-arch <125053864@upq.edu.mx>
2026-09-24 23:24:48 -0600

    subo dos archivos de la practica 3 de CofeCode Cocina

--- CofeCode/Cocina.js
+++ CofeCode/Cocina.js
@@ -1,3 +1,7 @@
+
+//Codigo Original Pegado de Cocina.js 
+
+
 let readline = require('readline');
 let teclado = readline.createInterface({
     input: process.stdin, 
@@ -5,94 +9,111 @@ let teclado = readline.createInterface({
 });
 
 let productos = [];
-let pedidos = [];
 
 function mostrarMenu(){
     console.log(`
-        Sistema de cocina
-
-        1. Gestionar productos
-        2. Consultar productos
-        3. Crear pedido 
-        4. Mostrar pedidos del cliente
-        5. Salir
-
+       MENU 
+       1. agregar
+       2. Editar
+       3. Eliminar
+       4. listar productos 
+       5. Buscar
+       6. Salir
     `);
+
     teclado.question("Seleccione una opcion: ", function(opcion)  {
+
         if (opcion == "1"){
-            menuProductos();
+            agregarProducto();
 
         }else if (opcion == "2"){
+            editarProducto();
 
+        }else if (opcion == "3"){
+            eliminarProducto();
+
+        }else if (opcion == "4") {
             listarProductos();
             mostrarMenu();
 
-        }else if (opcion == "3"){
-            crearPedido();
 
-        }else if (opcion == "4") {
-            mostrarPedidos();
-        }else if (opcion == "5"){
-            console.log("Saliendo del sistema");
+        }else if (opcion =="5") {
+            Buscar();
+
+        }else if (opcion == "6"){
+            console.log("Saliendo del menu");
             teclado.close();
+
         }else {
             console.log("Opcion invalida");
             mostrarMenu();
         }
     });
 }
-function menuProductos(){
-    console.log(`
-        GESTION DE PRODUCTOS 
-        1. AGREGAR
-        2. Editar
-        3. Eliminar
-        4. Listar Producto
-        5. Regresar
-        `);
-
-        teclado.question("Seleccione una opcion: ", function(opcion){
-        if(opcion =="1"){
-            agregarProducto();
-
-        }else if(opcion=="2"){
-            editarProducto();
-
-        }else if(opcion=="3"){
-            eliminarProducto();
-
-        }else if(opcion=="4"){
-            listarProductos();
-            menuProductos();
-
-        }else if(opcion=="5"){
-            mostrarMenu();
-        }else {
-
-            console.log("Opcion invalida");
-            menuProductos();
-        }
-        });
-        
-}
-
-
 
 function agregarProducto(){
     teclado.question("Nombre del producto: ", function(nombre){
+
         teclado.question("precio deel producto: ",function(precio){
-            let producto = {
+
+            teclado.question("Nombre de la categoria?: bebida o postre ", function(categorias){
+
+                revisarIngrediente()
+                .then(function){
+                    return prepararProducto(nombre);
+                })
+                .then(function(){
+
+                    let producto = {
                 nombre: nombre,
-                precio: Number(precio)
-            };
+                precio: Number(precio),
+                Categoria: categorias
+                    
+                };
+                
             productos.push(producto);
 
             console.log("producto agregado");
-            menuProductos();
+            mostrarMenu();
 
-        
+        })
+            .catch(function(error){
+                console.log(error);
+            
+            if(error()=="falta ingrediente"){
+                agregarProducto();
+            }else{
+                    mostrarMenu();
+                }
+
+            });
+
+        }else if(error == "Error en cocina"){
+            mostrarMenu();
+        }
+    });
+
+    });
+
         });
     });
+}
+
+function revisarIngrediente(){
+    return new promiseHooks(function(rosolver, rechazar){
+        let numero = Math.floor(Math.random()* 10)+1;
+
+        if(numero == 1){
+            rechazar("Falta ingrediente");
+
+        }else{
+            resolver();
+        }
+    });
+}
+
+prepararProducto(){
+    return new promise(function)
 }
 
 function editarProducto(){
@@ -103,20 +124,32 @@ function editarProducto(){
         let indice = numero -1;
        
         teclado.question("NUEVO nombre: ", function(nombre) {
+
             teclado.question("Nuevo precio: ", function(precio){
 
-                productos[indice].nombre = nombre;
-                productos[indice].precio = Number(precio);
+                teclado.question("Nueva categoria", function(categorias){
+
+                    productos[indice].nombre = nombre;
+                    productos[indice].precio = Number(precio);
+                    productos[indice].Categoria = categorias;
 
                 console.log("Producto editado.");
-                menuProductos();
+                mostrarMenu();
+
+
+
+
+
+                });
 
             });
         });
     });
 }
+
 function eliminarProducto(){
     listarProductos();
+
     teclado.question("ingrese el nuemero del a eliminar: ", function(numero){
 
         let indice = numero -1;
@@ -124,9 +157,10 @@ function eliminarProducto(){
         productos.splice(indice, 1);
 
         console.log("Producto eliminado.")
-        menuProductos();
+        mostrarMenu();
     })
 }
+
 function listarProductos(){
     if (productos.length == 0) {
         console.log("No hay productos registrados.");
@@ -134,7 +168,6 @@ function listarProductos(){
     } else {
         console.log(`
             PRODUCTOS REGISTRADOS
-
         `);
 
         for (let i =0; i < productos.length; i++){
@@ -145,79 +178,12 @@ function listarProductos(){
                 productos[i].precio
             );
         }
-
-        
     }
-}function crearPedido(){
-    listarProductos();
-
-    if(productos.length== 0){
-        mostrarMenu();
-    return;
-
-    }
-
-    teclado.question("Nombre del cliente: ", function(cliente){
-        teclado.question("Seleccione un producto: ",function(numero){
-
-            let indice = numero -1;
-            let producto = productos[indice];
-
-        
-        
-        teclado.question("ingresela la cantidad: ", function(cantidad){
-            cantidad = Number(cantidad);
-
-            let pedido ={
-                cliente: cliente,
-                producto: producto.nombre,
-                precio: producto.precio,
-                cantidad: cantidad,
-                total: producto.precio * cantidad
-            };
-            pedidos.push(pedido);
-
-            console.log(`
-                PEDIDO GUARDADO
-                Cliente: ${pedido.cliente}
-                Producto: ${pedido.producto}
-                precio: ${producto.precio}
-                Cantidad: $${pedido.cantidad}
-                Total: $${pedido.total}
-
-                 `);
-
-                 mostrarMenu();
-
-        });
-                
-        });
-        
-    });
-}
-function mostrarPedidos(){
-    if(pedidos.length ==0){
-        console.log("No hay pedidos registrados.");
-
-    } else {
-        console.log(`
-            PEDIDOS DEL CLIENTE
-            `);
-            for (let i = 0; i< pedidos.length; i++){
-                console.log(`
-                    pedido${i + 1}
-                    Cliente: ${pedidos[i].cliente}
-                    Producto: ${pedidos[i].producto}
-                    precio: ${pedidos[i].precio}
-                    Cantidad: ${pedidos[i].cantidad}
-                    Total: $${pedidos[i].total}
-                    `);
-            }
-    }
-
 }
 
 mostrarMenu();
+
+// Parte de Codigo pegado hecho en Trabajo.Unido 
 
 function Buscar(){
     console.log(`
@@ -226,32 +192,83 @@ function Buscar(){
         2. productos Caros 
         3. Bebidas
         4. Postres
+        5. Regresar
         
     `);
 
-teclado.cuestion("Seleccione una opcion de buscar",function(opcion) {
-    if(opcion =="1"){
+
+    teclado.question("Seleccione una opcion de busqueda: ",function(opcion1) {
+
+    if(opcion1 == "1"){
+        barato();
+        Buscar();
     
-    }else { 
-        if(opcion =="2"){
-        
-        }
+    } else if(opcion1 == "2"){
+        caro();
+        Buscar();
+
+    } else if(opcion1 == "3"){
+        bebidas();
+        Buscar();
+
+    } else if(opcion1 == "4"){
+        pasteles();
+        Buscar();
+
+    } else if(opcion1 == "5"){
+        mostrarMenu();
     }else {
-        if(opcion =="3"){
-
-        }
-    }else if(opcion=="4"){
-
-    }else if{
-        
-    console.log("Opcion invalida");
+            console.log("Opcion de busqueda invalida");
+            Buscar();
+            
     }
 
-
+    
 });
+//Ya apartir de aqui es donde lo deje 
+}
+function barato(){
+    let productosBaratos = productos.filter( productos => productos.precio <50 );
+    if (productosBaratos.length >0){
+        console.log(productosBaratos);
+    }else {
+        console.log("No hay productos que buscar ");
+
+    }
+    
+}
+function caro(){
+    let productosCaros = productos.filter( productos => productos.precio >50);
+    if (productosCaros.length >0){
+        console.log(productosCaros);
+
+    }else {
+        console.log("No hay productos caros ");
+    }
+
+}
+function bebidas(){
+    let productobebidas = productos.filter( productos => productos.Categoria === "bebida");
+    if (productobebidas.length >0){
+        console.log(productobebidas);
+
+    }else {
+        console.log("No hay bebidas ")
+    }
+
+}
+function pasteles(){
+    let productoPasteles =productos.filter(productos => productos.Categoria === "postre");
+
+    if(productoPasteles.length>0){
+        console.log(productoPasteles);
+    }else {
+        console.log("No hay pasteles");
+    }
 }
 
-let Barato = mostrarPedidos.filter(mostrarPedidos.precio < 50);
+let promesa = new Promise((resolver, rechazar)=>{
 
-
+    resolver("si se pudo ")
+})
 
--- CofeCode/practicasParaCofeCode.js
+++ CofeCode/practicasParaCofeCode.js
@@ -1,23 +1,109 @@
+let readline = require('readline');
+
+let teclado = readline.createInterface({
+    input: process.stdin, 
+    output: process.stdout
+});
+
+let manzanas = [
+    { color: "roja", cantidad: 5 },
+    { color: "verde", cantidad: 8 },
+    { color: "amarilla", cantidad: 3 },
+    { color: "rosa", cantidad: 6 }
+];
+let cafe= [];
 
 
-let uno = 1
 
-function intento(){
-    if(uno = 1){
-        console.log(promesa1);
-    }
+function revisarManzanas() {
+    return new Promise((resolve, reject) => {
+
+        console.log("Revisando manzanas...");
+
+        setTimeout(() => {
+        console.log("Pasaron 3 segundos");
+
+        if (manzanas.length > 0) {
+                resolve(manzanas);
+            } else {
+                reject("No hay manzanas");
+            }
+
+
+    }, 3000);
+
+
+            
+
+        
+    });
 }
 
-let promesa1 = new Promise((resolver, rechazar)=>{
+
+revisarManzanas()
+
+    .then((resultado) => {
+        console.log("¡Revisión terminada!");
+        console.log(resultado);
+    })
+    .catch((error) => {
+        console.log("Ocurrió un error:", error);
+    });
 
 
-    resolver("si se pudo ");
 
 
-});
+function randomaizer(){
+let numero = Math.floor(Math.random() * 2) + 1;
+    
+if (numero === 1) {
+    console.log("Salió uno");
+}
 
-let cafe = new Promise((resolver, rechazar) => {
+if (numero === 2) {
+    console.log("Salió dos");
 
-    rechazar("Falta azúcar");
+}
 
-});
+}
+
+randomaizer();
+
+function PreparandoCafe(){
+    return new Promise((siCafe,Nocafe)=>{
+
+        teclado.question("Que cafe es ", function(intentocafe){
+
+        let cafes= {
+            producto: intentocafe,
+            precio: 10
+
+        };
+
+        cafe.push(cafes);
+        
+
+    
+        console.log("haciendo cafe")
+
+        if(cafe.length>0){
+            siCafe(cafe);
+        }else {
+            Nocafe("No hay cafe");
+        }
+    });
+ });
+
+}
+
+PreparandoCafe()
+    .then((resultado1) => {
+        console.log("¡Revisión terminada!",
+        resultado1);
+        
+    })
+    .catch((error1) => {
+        console.log("Ocurrió un error:", error1);
+    });
+
+    
